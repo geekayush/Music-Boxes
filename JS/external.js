e032a65fe83b8255ea55bdab85e9249cd0ade9d5
geekayush <srivastavs61@gmail.com>
2017-06-18 03:20:58 +0530

    First commit/ Add files

---
+++ JS/external.js
@@ -0,0 +1,41 @@
+function cmyFunction() {
+    var cNote = document.getElementById("cAudio");
+    cNote.currentTime = 0;
+    cNote.play();
+}
+
+function dmyFunction() {
+    var dNote = document.getElementById("dAudio");
+    dNote.currentTime = 0;
+    dNote.play();
+}
+
+function emyFunction() {
+    var eNote = document.getElementById("eAudio");
+    eNote.currentTime = 0;
+    eNote.play();
+}
+
+function fmyFunction() {
+    var fNote = document.getElementById("fAudio");
+    fNote.currentTime = 0;
+    fNote.play();
+}
+
+function gmyFunction() {
+    var gNote = document.getElementById("gAudio");
+    gNote.currentTime = 0;
+    gNote.play();
+}
+
+function amyFunction() {
+    var aNote = document.getElementById("aAudio");
+    aNote.currentTime = 0;
+    aNote.play();
+}
+
+function bmyFunction() {
+    var bNote = document.getElementById("bAudio");
+    bNote.currentTime = 0;
+    bNote.play();
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# MUSIC-BOXES
+
+Made using HTML, CSS and JS.
+A simple instrument that you can use to make music!
